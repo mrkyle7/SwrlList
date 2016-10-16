d58f2c72185b215eca299d21d1999ded848bc7be
Kyle Harrison <kylejharrison@gmail.com>
2016-10-16 20:52:33 +0100

    add wakeUpDevice to tests for travis emulator

--- app/src/androidTest/java/co/swrl/swrllist/MainActivityTest.java
+++ app/src/androidTest/java/co/swrl/swrllist/MainActivityTest.java
@@ -1,11 +1,14 @@
 package co.swrl.swrllist;
 
+import android.app.Activity;
 import android.support.test.espresso.matcher.ViewMatchers;
 import android.support.test.filters.LargeTest;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.view.WindowManager;
 
 import org.hamcrest.Matchers;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -19,11 +22,29 @@ import static org.junit.Assert.*;
 @RunWith(AndroidJUnit4.class)
 @LargeTest
 public class MainActivityTest {
+    Activity activity = null;
     @Rule
     public ActivityTestRule mainActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Before
+    public void setUp() {
+        wakeUpDevice();
+    }
 
     @Test
     public void helloWorldTest(){
         onView(withId(R.id.textView)).check(matches(withText("Hello World!")));
+    }
+
+    private void wakeUpDevice() {
+        activity = mainActivityTestRule.getActivity();
+        Runnable wakeUpDevice = new Runnable() {
+            public void run() {
+                activity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON |
+                        WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED |
+                        WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+            }
+        };
+        activity.runOnUiThread(wakeUpDevice);
     }
 }
