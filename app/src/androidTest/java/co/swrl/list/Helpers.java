9a0833416129a893348b7cf66cf9b44871f087b0
Kyle Harrison <kylejharrison@gmail.com>
2016-11-22 16:33:38 +0530

    separate test helpers

---
+++ app/src/androidTest/java/co/swrl/list/Helpers.java
@@ -0,0 +1,79 @@
+package co.swrl.list;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.rule.ActivityTestRule;
+import android.view.WindowManager;
+
+import static android.support.test.espresso.Espresso.onData;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.instanceOf;
+import static org.hamcrest.Matchers.is;
+
+class Helpers {
+    private Helpers (){}
+
+    static void clearAllSettings() {
+        Context applicationContext = InstrumentationRegistry.getTargetContext();
+        SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(applicationContext);
+        settings.edit().clear().apply();
+    }
+
+    static Activity launchAndAvoidWhatsNewDialog(ActivityTestRule testRule) {
+        setSavedVersionToHugeNumber();
+        return launchAndWakeUpActivity(testRule);
+    }
+
+    static void setSavedVersionToHugeNumber() {
+        Context applicationContext = InstrumentationRegistry.getTargetContext();
+        SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(applicationContext);
+
+        int currentVersion = Integer.MAX_VALUE;
+        settings.edit().putInt(String.valueOf(R.string.pkey_version_number), currentVersion).apply();
+    }
+    static Activity restartActivity(Activity activity, ActivityTestRule testRule) {
+        stopActivity(activity);
+        return launchAndWakeUpActivity(testRule);
+    }
+
+    static Activity launchAndWakeUpActivity(ActivityTestRule testRule) {
+        testRule.launchActivity(null);
+        Activity activity = testRule.getActivity();
+        wakeUpDevice(activity);
+        return activity;
+    }
+    private static void wakeUpDevice(final Activity activity) {
+        Runnable wakeUpDevice = new Runnable() {
+            public void run() {
+                activity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON |
+                        WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED |
+                        WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+            }
+        };
+        activity.runOnUiThread(wakeUpDevice);
+    }
+    private static void stopActivity(Activity activity) {
+        activity.finish();
+    }
+    static void addSwrlsToList(Swrl[] swrls) {
+        for (Swrl swrl : swrls) {
+            onView(withId(R.id.addItemEditText)).perform(typeText(swrl.getTitle()));
+            onView(withId(R.id.addItemButton)).perform(click());
+            onData(allOf(is(instanceOf(Swrl.class)), equalTo(swrl))).check(matches(isDisplayed()));
+        }
+    }
+    static void purgeDatabase(Context activity) {
+        SQLiteCollectionManager db = new SQLiteCollectionManager(activity);
+        db.permanentlyDeleteAll();
+    }
+}
--- app/src/androidTest/java/co/swrl/list/ListOperationTest.java
+++ app/src/androidTest/java/co/swrl/list/ListOperationTest.java
@@ -1,5 +1,6 @@
 package co.swrl.list;
 
+import co.swrl.list.Helpers;
 import android.app.Activity;
 import android.content.Context;
 import android.content.SharedPreferences;
@@ -29,6 +30,13 @@ import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.action.ViewActions.pressImeActionButton;
 import static android.support.test.espresso.action.ViewActions.pressKey;
 import static android.support.test.espresso.action.ViewActions.typeText;
+import static co.swrl.list.Helpers.addSwrlsToList;
+import static co.swrl.list.Helpers.clearAllSettings;
+import static co.swrl.list.Helpers.launchAndAvoidWhatsNewDialog;
+import static co.swrl.list.Helpers.launchAndWakeUpActivity;
+import static co.swrl.list.Helpers.purgeDatabase;
+import static co.swrl.list.Helpers.restartActivity;
+import static co.swrl.list.Helpers.setSavedVersionToHugeNumber;
 import static org.hamcrest.Matchers.allOf;
 import static org.hamcrest.Matchers.containsString;
 
@@ -47,12 +55,11 @@ import static org.hamcrest.Matchers.not;
 
 @RunWith(AndroidJUnit4.class)
 @LargeTest
-public class ListActivityIntegrationTest {
+public class ListOperationTest {
     private Activity activity = null;
     private static final EspressoKey ENTER_KEY = new EspressoKey.Builder().withKeyCode(KeyEvent.KEYCODE_ENTER).build();
     private static final Swrl THE_MATRIX = new Swrl("The Matrix");
     private static final Swrl THE_MATRIX_RELOADED = new Swrl("The Matrix Reloaded");
-    private static final Swrl THE_MATRIX_REVOLUTIONS = new Swrl("The Matrix Revolutions");
 
     @Rule
     public ActivityTestRule listActivityActivityTestRule = new ActivityTestRule<>(ListActivity.class, false, false);
@@ -65,17 +72,12 @@ public class ListActivityIntegrationTest {
     @After
     public void tearDown() {
         clearAllSettings();
-        purgeDatabase();
-    }
-
-    private void purgeDatabase() {
-        SQLiteCollectionManager db = new SQLiteCollectionManager(activity);
-        db.permanentlyDeleteAll();
+        purgeDatabase(activity);
     }
 
     @Test
     public void whatsNewDialogIsOnlyShownOnFirstLaunchOfActivity() throws Exception {
-        launchAndWakeUpActivity();
+        activity = activity = launchAndWakeUpActivity(listActivityActivityTestRule);
 
         onView(withId(R.id.whatsNew)).inRoot(isDialog()).check(matches(isDisplayed()));
         onView(withId(R.id.whatsNewMessage)).check(matches(withText(containsString("Version"))));
@@ -85,7 +87,7 @@ public class ListActivityIntegrationTest {
         onView(withId(R.id.whatsNew)).check(doesNotExist());
         onView(withId(R.id.activity_list)).check(matches(isCompletelyDisplayed()));
 
-        restartActivity();
+        restartActivity(activity, listActivityActivityTestRule);
 
         onView(withId(R.id.activity_list)).check(matches(isCompletelyDisplayed()));
     }
@@ -93,14 +95,14 @@ public class ListActivityIntegrationTest {
     @Test
     public void whatsNewDialogIsNotShownWhenNotNewVersion() throws Exception {
         setSavedVersionToHugeNumber();
-        launchAndWakeUpActivity();
+        activity = launchAndWakeUpActivity(listActivityActivityTestRule);
 
         onView(withId(R.id.activity_list)).check(matches(isCompletelyDisplayed()));
     }
 
     @Test
     public void newItemsInTheListAreAddedOnTopAndTextInputIsClearedAndFocused() throws Exception {
-        avoidWhatsNewDialog();
+        activity = launchAndAvoidWhatsNewDialog(listActivityActivityTestRule);
 
         //With IME Action Button
         onView(withId(R.id.addItemEditText))
@@ -140,7 +142,7 @@ public class ListActivityIntegrationTest {
 
     @Test
     public void cannotAddDuplicateItems() throws Exception {
-        avoidWhatsNewDialog();
+        activity = launchAndAvoidWhatsNewDialog(listActivityActivityTestRule);
 
         addSwrlsToList(new Swrl[]{THE_MATRIX});
 
@@ -153,7 +155,7 @@ public class ListActivityIntegrationTest {
 
     @Test
     public void emptyTextIsNotAddedToTheList() throws Exception {
-        avoidWhatsNewDialog();
+        activity = launchAndAvoidWhatsNewDialog(listActivityActivityTestRule);
 
         Swrl emptySwrl = new Swrl("");
 
@@ -168,18 +170,18 @@ public class ListActivityIntegrationTest {
 
     @Test
     public void itemsOnTheListArePersistedAfterRestart() throws Exception {
-        avoidWhatsNewDialog();
+        activity = launchAndAvoidWhatsNewDialog(listActivityActivityTestRule);
 
         addSwrlsToList(new Swrl[]{THE_MATRIX});
 
-        restartActivity();
+        activity = restartActivity(activity, listActivityActivityTestRule);
 
         onData(allOf(is(instanceOf(Swrl.class)), equalTo(THE_MATRIX))).check(matches(isDisplayed()));
     }
 
     @Test
     public void canDeleteAndReAddItemsOnTheList() throws Exception {
-        avoidWhatsNewDialog();
+        activity = launchAndAvoidWhatsNewDialog(listActivityActivityTestRule);
 
         addSwrlsToList(new Swrl[]{THE_MATRIX, THE_MATRIX_RELOADED});
 
@@ -191,7 +193,7 @@ public class ListActivityIntegrationTest {
         onView(withId(R.id.itemListView)).check(matches(numberOfChildren(is(1))));
         onData(allOf(is(instanceOf(Swrl.class)), equalTo(THE_MATRIX_RELOADED))).check(matches(isDisplayed()));
 
-        restartActivity();
+        activity = restartActivity(activity, listActivityActivityTestRule);
 
         onView(withId(R.id.itemListView)).check(matches(not(exists(equalTo(THE_MATRIX)))));
         onData(allOf(is(instanceOf(Swrl.class)), equalTo(THE_MATRIX_RELOADED))).check(matches(isDisplayed()));
@@ -203,66 +205,14 @@ public class ListActivityIntegrationTest {
         onData(allOf(is(instanceOf(Swrl.class)), equalTo(THE_MATRIX_RELOADED))).check(matches(isDisplayed()));
         onView(withId(R.id.itemListView)).check(matches(numberOfChildren(is(2))));
 
-        restartActivity();
+        activity = restartActivity(activity, listActivityActivityTestRule);
 
         onData(allOf(is(instanceOf(Swrl.class)), equalTo(THE_MATRIX))).check(matches(isDisplayed()));
         onData(allOf(is(instanceOf(Swrl.class)), equalTo(THE_MATRIX_RELOADED))).check(matches(isDisplayed()));
         onView(withId(R.id.itemListView)).check(matches(numberOfChildren(is(2))));
     }
 
-    private void addSwrlsToList(Swrl[] swrls) {
-        for (Swrl swrl : swrls) {
-            onView(withId(R.id.addItemEditText)).perform(typeText(swrl.getTitle()));
-            onView(withId(R.id.addItemButton)).perform(click());
-            onData(allOf(is(instanceOf(Swrl.class)), equalTo(swrl))).check(matches(isDisplayed()));
-        }
-    }
 
-    private void restartActivity() {
-        stopActivity();
-        launchAndWakeUpActivity();
-    }
-
-    private void avoidWhatsNewDialog() {
-        setSavedVersionToHugeNumber();
-        launchAndWakeUpActivity();
-    }
-
-    private void clearAllSettings() {
-        Context applicationContext = InstrumentationRegistry.getTargetContext();
-        SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(applicationContext);
-        settings.edit().clear().apply();
-    }
-
-    private void setSavedVersionToHugeNumber() {
-        Context applicationContext = InstrumentationRegistry.getTargetContext();
-        SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(applicationContext);
-
-        int currentVersion = Integer.MAX_VALUE;
-        settings.edit().putInt(String.valueOf(R.string.pkey_version_number), currentVersion).apply();
-    }
-
-    private void launchAndWakeUpActivity() {
-        listActivityActivityTestRule.launchActivity(null);
-        activity = listActivityActivityTestRule.getActivity();
-        wakeUpDevice(activity);
-    }
-
-    private void wakeUpDevice(final Activity activity) {
-        Runnable wakeUpDevice = new Runnable() {
-            public void run() {
-                activity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON |
-                        WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED |
-                        WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-            }
-        };
-        activity.runOnUiThread(wakeUpDevice);
-    }
-
-    private void stopActivity() {
-        activity.finish();
-        activity = null;
-    }
 
     private static Matcher<View> exists(final Matcher<Swrl> dataMatcher) {
         return new TypeSafeMatcher<View>() {
@@ -290,7 +240,7 @@ public class ListActivityIntegrationTest {
         };
     }
 
-    public static Matcher<View> numberOfChildren(final Matcher<Integer> numChildrenMatcher) {
+    private static Matcher<View> numberOfChildren(final Matcher<Integer> numChildrenMatcher) {
         return new TypeSafeMatcher<View>() {
 
             /**
