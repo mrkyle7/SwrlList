0ab1c0863a760a36b0b67de30c32f6d1b3f96375
Kyle Harrison <kylejharrison@gmail.com>
2016-12-28 12:48:02 +0000

    remove floating button from view

--- app/src/main/java/co/swrl/list/ui/ViewActivity.java
+++ app/src/main/java/co/swrl/list/ui/ViewActivity.java
@@ -52,7 +52,6 @@ public class ViewActivity extends AppCompatActivity {
     private void setupView() {
         setUpToolbar();
         setupPager();
-        addDoneButton();
     }
 
     private void setUpToolbar() {
@@ -95,17 +94,6 @@ public class ViewActivity extends AppCompatActivity {
             @Override
             public void onPageScrollStateChanged(int state) {
 
-            }
-        });
-    }
-
-    private void addDoneButton() {
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
             }
         });
     }
